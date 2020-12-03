551d75ed86b4f2d41f6bf829f17f13a97012213f
abhishekgupta-99 <abhishekgupta97023@gmail.com>
2020-12-03 18:45:22 +0530

    Initial commit

---
+++ app/src/main/java/com/abhishek/SEM6/rooms.java
@@ -0,0 +1,21 @@
+package com.abhishek.SEM6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class rooms extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.rooms);
+    }
+
+    public void create_room(View view) {
+    }
+
+    public void Join_Room(View view) {
+    }
+}
